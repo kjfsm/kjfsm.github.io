c016f19704b2ed6b555dcf864de57ea11ee19bfa
kjfsm <kjfsm@users.noreply.github.com>
2025-04-02 08:37:00 +0000

    feat: コンタクトページを追加

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,4 +1,5 @@
 "use client";
+import ContactMailIcon from "@mui/icons-material/ContactMail";
 import EmojiPeopleIcon from "@mui/icons-material/EmojiPeople";
 import HomeIcon from "@mui/icons-material/Home";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -26,6 +27,7 @@ export default function TemporaryDrawer() {
     { icon: <HomeIcon />, text: "ホーム", path: "/" },
     { icon: <EmojiPeopleIcon />, text: "自分について", path: "/about" },
     { icon: <WindowIcon />, text: "Windows", path: "/windows" },
+    { icon: <ContactMailIcon />, text: "お問い合わせ", path: "/contact" },
   ];
 
   const DrawerList = (
