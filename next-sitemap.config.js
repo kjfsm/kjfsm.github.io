77811769f5560c2bb3d02955c8d1f7571a85b57b
kjfsm <kjfsm@users.noreply.github.com>
2025-04-24 15:54:27 +0000

    sitemapの追加

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.kjfsm.net",
+  generateRobotsTxt: true,
+  sitemapSize: 7000, // 任意
+  changefreq: "daily",
+  priority: 0.7,
+};
